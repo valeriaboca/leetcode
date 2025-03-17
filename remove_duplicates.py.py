22d632b53c0314ccf351ece99c919aa0f19b9db7
Valeria Boca <vboca660@gmail.com>
2025-03-16 21:46:36 -0400

    initial commit

---
+++ remove_duplicates.py.py
@@ -0,0 +1,12 @@
+from typing import List
+
+# 26
+def removeDuplicates( nums: List[int]) -> int:
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
+    
+print(removeDuplicates([1,1,2,3,4,4]))
