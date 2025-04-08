9946702bc1982f53062b45cd3a5c27ff1b44bc43
Valeria Boca <vboca660@gmail.com>
2025-04-07 21:49:31 -0500

    #206

--- linked_list/reverse_linked_list.py
+++ linked_list/reverse_linked_list.py
@@ -1,2 +1,10 @@
 
 # 206. Reverse Linked List
+# def reverseList(head: Optional[ListNode]) -> Optional[ListNode]:
+#        prev, curr = None, head
+#        while curr:
+#            nextPointer = curr.next
+#            curr.next = prev
+#            prev = curr
+#            curr = nextPointer
+#        return prev
