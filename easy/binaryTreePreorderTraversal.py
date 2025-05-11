9025cf00fd4ff63bbcc0afc0cc17717bf9f33a12
Valeria Boca <vboca660@gmail.com>
2025-05-10 22:01:09 -0400

    # 144

---
+++ easy/binaryTreePreorderTraversal.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+from collections import deque
+
+# 144. Binary Tree Preorder Traversal\
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+
+    while queue and i < len(values):
+        current = queue.popleft()
+        
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+
+    return root
+
+def preorderTraversal(root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if root is None:
+            return result
+
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
+
+root_values = [1,2,3,4,5,None,8,None,None,6,7,9]
+root = build_tree(root_values)
+
+root_list = [1,2,3,4,5,None,8,None,None,6,7,9]
+root = build_tree(root_list)
+print(preorderTraversal(root))
